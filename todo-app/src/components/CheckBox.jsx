6ec3dd4546d301cf3fe91e2323a807d948d1e749
JG <wprkf1005@gmail.com>
2025-06-10 16:30:28 +0900

    3

--- todo-app/src/components/CheckBox.jsx
+++ todo-app/src/components/CheckBox.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
 
-const CheckBox = () => {
+const CheckBox = ({ todos, item, index, setTodos }) => {
+  const handleCheck = (e) => {
+    const isChecked = e.target.checked;
+
+    const updatedTodos = todos.map((data, idx) => {
+      if (idx === index) {
+        
+        if (isChecked) {
+          return <del>{getTextOnly(data)}</del>;
+        } else {
+          
+          return getTextOnly(data);
+        }
+      }
+      return data;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  
+  const getTextOnly = (data) => {
+    return typeof data === 'string' ? data : data.props?.children || '';
+  };
+
   return (
-    <div>CheckBox</div>
-  )
-}
+    <div>
+      <input type="checkbox" onChange={handleCheck} />
+      {item}
+    </div>
+  );
+};
 
-export default CheckBox
+export default CheckBox;
