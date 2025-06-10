e2676c684fb4a3e9a111bccf7af5cdd69748db1d
곽희돈 <khdon0405789@gmail.com>
2025-06-10 17:55:06 +0900

    dd

--- todo-app/src/components/Update.jsx
+++ todo-app/src/components/Update.jsx
@@ -1,23 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 
-const Update = () => {
-    
-
-
-  const [] = useState([]);
-  const upRef = useRef("");
-  let a = setState();
-
+const Update = ({todos, setTodos, item, index}) => {
+  const [isEditing, setIsEditing] = useState(false);   //수정 모드 여부
+  const textRef = useRef("");
 
   return (
-    
-    <div>
+    <button onClick={()=>{
+      if(!isEditing){
+        setIsEditing(!isEditing);
+        let newTodos = [...todos];
+        newTodos[index] = <input type='text'/>
+        setTodos(newTodos);
+      }else{
+        setIsEditing(!isEditing)
+      }
+    }}>{isEditing?"수정완료":"내용 수정하기"}
+    </button>
+  );
 
- 
-    </div>
-    
-  )
 }
 
 export default Update
